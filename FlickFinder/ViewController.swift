8d77895b7449793078fbdf0846f6985ea6e75dd9
Logan Yang <logan1934@gmail.com>
2015-11-04 20:30:12 -0600

    initial commit

---
+++ FlickFinder/ViewController.swift
@@ -0,0 +1,361 @@
+//
+//  ViewController.swift
+//  FlickFinder
+//
+//  Created by Logan Yang on 11/3/15.
+//  Copyright © 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+/* 1 - Define constants */
+let BASE_URL = "https://api.flickr.com/services/rest/"
+let METHOD_NAME = "flickr.photos.search"
+let API_KEY = "ENTER YOUR KEY"
+let TEXT = ""
+let BBOX = "-180, -90, 180, 90"
+let EXTRAS = "url_m"
+let DATA_FORMAT = "json"
+let NO_JSON_CALLBACK = "1"
+let BOUNDING_BOX_HALF_WIDTH = 1.0
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var phraseTextField: UITextField!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+    @IBOutlet weak var imageName: UILabel!
+    
+    var tapRecognizer: UITapGestureRecognizer!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tapRecognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+        tapRecognizer?.numberOfTapsRequired = 1
+        // in order to make keyboard hide work, set textField delegate
+        // and set delegate method "textFieldShouldReturn"
+        phraseTextField.delegate = self
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.addKeyboardDismissRecognizer()
+        self.subscribeToKeyboardNotifications()
+    }
+
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.removeKeyboardDismissRecognizer()
+        self.unsubscribeToKeyboardNotifications()
+    }
+
+    @IBAction func phraseSearch(sender: AnyObject) {
+        /* 2 - API method arguments */
+        let methodArguments = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "text": phraseTextField.text!,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        
+        /* 3 - Initialize session and url */
+        searchFlickr(methodArguments)
+    }
+
+    @IBAction func locationSearch(sender: AnyObject) {
+        let lonlat = boundingBoxString()
+        
+        let methodArguments = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "bbox": lonlat,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        
+        searchFlickr(methodArguments)
+    }
+    
+    func searchFlickr(methodArguments: [String: AnyObject]) {
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(methodArguments)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        getImageFromFlickr(session, url: url, request: request, methodArguments: methodArguments)
+    }
+    
+    func getImageFromFlickr(session: NSURLSession, url: NSURL, request: NSURLRequest, methodArguments: [String: AnyObject]){
+        /* 4 - Initialize task for getting data */
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* 5 - Check for a successful response */
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* 6 - Parse the data (i.e. convert the data to JSON and look for values!) */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error (stat != ok)? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Are the "photos" and "photo" keys in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? NSDictionary else {
+                    print("Cannot find keys 'photos' in \(parsedResult)")
+                    return
+            }
+            
+            let pageNumber = self.genRandomPage(photosDictionary as! [String : AnyObject], parsedResult: parsedResult)
+            self.getImageFromFlickrWithPage(methodArguments, pageNumber: pageNumber)
+        }
+        /* 9 - Resume (execute) the task */
+        task.resume()
+    }
+    
+    func getImageFromFlickrWithPage(methodArguments: [String : AnyObject], pageNumber: Int) {
+        
+        /* Add the page to the method's arguments */
+        var withPageDictionary = methodArguments
+        withPageDictionary["page"] = pageNumber
+        
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(withPageDictionary)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* Parse the data! */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error (stat != ok)? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is the "photos" key in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? NSDictionary else {
+                print("Cannot find key 'photos' in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is the "total" key in photosDictionary? */
+            guard let totalPhotosVal = (photosDictionary["total"] as? NSString)?.integerValue else {
+                print("Cannot find key 'total' in \(photosDictionary)")
+                return
+            }
+            
+            if totalPhotosVal > 0 {
+                
+                /* GUARD: Is the "photo" key in photosDictionary? */
+                guard let photosArray = photosDictionary["photo"] as? [[String: AnyObject]] else {
+                    print("Cannot find key 'photo' in \(photosDictionary)")
+                    return
+                }
+                
+                let randomPhotoIndex = Int(arc4random_uniform(UInt32(photosArray.count)))
+                let photoDictionary = photosArray[randomPhotoIndex] as [String: AnyObject]
+                let photoTitle = photoDictionary["title"] as? String /* non-fatal */
+                
+                /* GUARD: Does our photo have a key for 'url_m'? */
+                guard let imageUrlString = photoDictionary["url_m"] as? String else {
+                    print("Cannot find key 'url_m' in \(photoDictionary)")
+                    return
+                }
+                
+                let imageURL = NSURL(string: imageUrlString)
+                if let imageData = NSData(contentsOfURL: imageURL!) {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        
+                        self.imageView.image = UIImage(data: imageData)
+                        
+                        if methodArguments["bbox"] != nil {
+                            if let photoTitle = photoTitle {
+                                self.imageName.text = "\(photoTitle)"
+                            } else {
+                                self.imageName.text = "(Untitled)"
+                            }
+                        } else {
+                            self.imageName.text = photoTitle ?? "(Untitled)"
+                        }
+                    })
+                } else {
+                    print("Image does not exist at \(imageURL)")
+                }
+            } else {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.imageName.text = "No Photos Found. Search Again."
+                    self.imageView.image = nil
+                })
+            }
+        }
+        
+        task.resume()
+    }
+
+    
+    /* Helper functions */
+    // Given a dictionary of parameters, convert to a string for a url
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+            
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+    }
+    
+    // Create a bounding box for lat/lon search
+    func boundingBoxString() -> String {
+        
+        let lat = (latitudeTextField.text! as NSString).doubleValue
+        let lon = (longitudeTextField.text! as NSString).doubleValue
+        
+        return "\(lon - BOUNDING_BOX_HALF_WIDTH),\(lat - BOUNDING_BOX_HALF_WIDTH),\(lon + BOUNDING_BOX_HALF_WIDTH),\(lat + BOUNDING_BOX_HALF_WIDTH)"
+    }
+    
+    // Generate a random page number. Flickr can only return at most 40 pages
+    func genRandomPage(photosDictionary: [String: AnyObject], parsedResult: AnyObject!) -> Int {
+        guard let totalPages = photosDictionary["pages"] as? Int else {
+            print("Cannot find keys 'pages' in \(parsedResult)")
+            return 1
+        }
+        let pageLimit = min(totalPages, 40)
+        let randomPage = Int(arc4random_uniform(UInt32(pageLimit) + 1))
+        return randomPage
+    }
+    
+    /* =====================================================================
+     * Functional stubs for handling UI problems
+     * ===================================================================== */
+    
+    
+    // 1 - Dismissing the keyboard
+    func addKeyboardDismissRecognizer(){
+        view.addGestureRecognizer(tapRecognizer!)
+    }
+    
+    func removeKeyboardDismissRecognizer(){
+        view.removeGestureRecognizer(tapRecognizer!)
+    }
+    
+    func handleSingleTap(recognizer: UITapGestureRecognizer){
+        view.endEditing(true)
+    }
+    
+    // textField delegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    // 2 - Shift the keyboard up and down
+    func subscribeToKeyboardNotifications(){
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+
+    func unsubscribeToKeyboardNotifications(){
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    /// when the keyboardWillShow notification is received, shift the view's frame up
+    func keyboardWillShow(notification: NSNotification) {
+        view.frame.origin.y = -getKeyboardHeight(notification)
+        
+    }
+    
+    /// when the keyboardWillHide notification is received, shift the view's frame down
+    func keyboardWillHide(notification: NSNotification) {
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+    }
+    
+}
+
